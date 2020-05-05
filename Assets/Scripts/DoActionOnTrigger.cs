d074a5bf4826781ced0f2b4ce393bce725d96090
Jirinrin <jiri.ei@hotmail.com>
2020-05-05 14:56:47 +0200

    Initial commit

---
+++ Assets/Scripts/DoActionOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class DoActionOnTrigger : MonoBehaviour
+{
+    public UnityEvent Event;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Event?.Invoke();
+    }
+}
