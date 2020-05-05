4c6c61ac8f0a9ae21802833b6cb6d6816274b6ac
Jirinrin <jiri.ei@hotmail.com>
2020-05-05 21:53:13 +0200

    Get rain reverse system working

--- Assets/Scripts/RainSystem.cs
+++ Assets/Scripts/RainSystem.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(ReverseParticleSystem))]
 public class RainSystem : MonoBehaviour
 {
     private float rot = 0f;
     private ParticleSystem ps;
     private ParticleSystem.ShapeModule sh;
+    private ReverseParticleSystem rv;
 
     private const float distanceToCamera = 30f;
 
@@ -16,18 +18,31 @@ public class RainSystem : MonoBehaviour
     {
         ps = GetComponent<ParticleSystem>();
         sh = ps.shape;
+        rv = GetComponent<ReverseParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rot = Time.time * 40;
-        sh.rotation = new Vector3(0, 0, rot);
-        var rotRad = Mathf.Deg2Rad * rot;
-        var pos = Camera.main.transform.position;
-        pos.x += Mathf.Cos(rotRad+Mathf.PI*0.5f ) * 30;
-        pos.y += Mathf.Sin(rotRad+Mathf.PI*0.5f ) * 30;
-        pos.z = transform.position.z;
-        sh.position = pos;
+        if (Time.time > 6f)
+        {
+            //Debug.Log("YOoooo");
+            rv.StopReverse();
+        }
+        else if (Time.time > 3f)
+        {
+            rv.StartReverse();
+            //ps.speed
+        }
+        
+
+        //rot = Time.time * 40;
+        //sh.rotation = new Vector3(0, 0, rot);
+        //var rotRad = Mathf.Deg2Rad * rot;
+        //var pos = Camera.main.transform.position;
+        //pos.x += Mathf.Cos(rotRad+Mathf.PI*0.5f ) * 30;
+        //pos.y += Mathf.Sin(rotRad+Mathf.PI*0.5f ) * 30;
+        //pos.z = transform.position.z;
+        //sh.position = pos;
     }
 }
---
+++ Assets/Scripts/ReverseParticleSystem.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseParticleSystem : MonoBehaviour
+{
+    ParticleSystem[] particleSystems;
+
+    float[] simulationTimes;
+
+    private bool reverseActive = false;
+
+    public float simulationSpeedScale = 1.0f;
+
+    void Initialize()
+    {
+        particleSystems = GetComponentsInChildren<ParticleSystem>(false);
+        simulationTimes = new float[particleSystems.Length];
+    }
+
+    void OnEnable()
+    {
+        if (particleSystems == null)
+        {
+            Initialize();
+        }
+
+        for (int i = 0; i < simulationTimes.Length; i++)
+            simulationTimes[i] = 0.0f;
+        //particleSystems[0].Simulate(startTime, true, false, true);
+    }
+    void Update()
+    {
+        if (reverseActive)
+        {
+            particleSystems[0].Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            for (int i = particleSystems.Length - 1; i >= 0; i--)
+            {
+                //bool useAutoRandomSeed = particleSystems[i].useAutoRandomSeed;
+                //particleSystems[i].useAutoRandomSeed = false;
+
+                particleSystems[i].Play(false);
+
+                simulationTimes[i] -= (Time.deltaTime * particleSystems[i].main.simulationSpeed) * simulationSpeedScale;
+
+                particleSystems[i].Simulate(simulationTimes[i], false, false, true);
+
+                //particleSystems[i].useAutoRandomSeed = useAutoRandomSeed;
+
+                if (simulationTimes[i] < 0.0f)
+                {
+                    particleSystems[i].Play(false);
+                    particleSystems[i].Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+                }
+            }
+        }
+        else
+        {
+            for (int i = particleSystems.Length - 1; i >= 0; i--)
+                simulationTimes[i] = particleSystems[i].time;
+        }
+    }
+
+    public void StartReverse()
+    {
+        if (reverseActive)
+            return;
+
+        Debug.Log("start!!");
+
+        reverseActive = true;
+    }
+
+    public void StopReverse()
+    {
+        if (!reverseActive)
+            return;
+
+        Debug.Log("reverse!!");
+
+        this.reverseActive = false;
+
+        for (int i = particleSystems.Length - 1; i >= 0; i--)
+        {
+            particleSystems[i].Simulate(simulationTimes[i], false, false, true);
+            particleSystems[i].Play(false);
+        }
+    }
+}
+
+//// http://www.mirzabeig.com/tutorials/rewinding-particle-systems/
+//public class ReverseParticleSystem : MonoBehaviour
+//{
+//    ParticleSystem[] particleSystems;
+
+//    float[] simulationTimes;
+
+//    public float startTime = 2.0f;
+//    public float simulationSpeedScale = 1.0f;
+
+//    void Initialize()
+//    {
+//        particleSystems = GetComponentsInChildren<ParticleSystem>(false);
+//        simulationTimes = new float[particleSystems.Length];
+//    }
+
+//    void OnEnable()
+//    {
+//        if (particleSystems == null)
+//        {
+//            Initialize();
+//        }
+
+//        for (int i = 0; i < simulationTimes.Length; i++)
+//            simulationTimes[i] = 0.0f;
+//        particleSystems[0].Simulate(startTime, true, false, true);
+//    }
+//    void Update()
+//    {
+//        particleSystems[0].Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+//        for (int i = particleSystems.Length - 1; i >= 0; i--)
+//        {
+//            bool useAutoRandomSeed = particleSystems[i].useAutoRandomSeed;
+//            particleSystems[i].useAutoRandomSeed = false;
+
+//            particleSystems[i].Play(false);
+
+//            float deltaTime = particleSystems[i].main.useUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+//            simulationTimes[i] -= (deltaTime * particleSystems[i].main.simulationSpeed) * simulationSpeedScale;
+
+//            float currentSimulationTime = startTime + simulationTimes[i];
+//            particleSystems[i].Simulate(currentSimulationTime, false, false, true);
+
+//            particleSystems[i].useAutoRandomSeed = useAutoRandomSeed;
+
+//            if (currentSimulationTime < 0.0f)
+//            {
+//                particleSystems[i].Play(false);
+//                particleSystems[i].Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+//            }
+//        }
+//    }
+//}
