9d7f56cfecee082e29eb20c3b80784de3054d976
Jirinrin <jiri.ei@hotmail.com>
2020-06-08 23:00:59 +0200

    Improve changing rain direction

--- Assets/Scripts/RainSystem.cs
+++ Assets/Scripts/RainSystem.cs
@@ -1,72 +1,70 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening.Core;
 
 [RequireComponent(typeof(ParticleSystem))]
 [RequireComponent(typeof(ReverseParticleSystemSimple))]
 public class RainSystem : MonoBehaviour
 {
-    public float rainRotation;
-    
-    private ParticleSystem ps;
-    private ParticleSystem.ShapeModule sh;
-    private ReverseParticleSystemSimple rv;
-    private Camera cam;
+    [SerializeField] private float rainChangeDuration = 1f;
+    [SerializeField] public float maxRainAngle = 1.57f;
 
-    private const float distanceToCamera = 35f;
+    private float _rainRotation;
+    public float rainRotation
+    {
+        get => _rainRotation;
+        private set
+        {
+            _rainRotation = value;
+            _psShape.rotation = new Vector3(0, 0, value*Mathf.Rad2Deg);
+        }
+    }
+
+    private ParticleSystem _particleSystem;
+    private ParticleSystem.ShapeModule _psShape;
+    private ReverseParticleSystemSimple _reverseParticleSystem;
+    private Camera _camera;
+
+    private const float DistanceToCamera = 35f;
 
     // Start is called before the first frame update
     private void Start()
     {
-        cam = Camera.main;
-        ps = GetComponent<ParticleSystem>();
-        sh = ps.shape;
-        rv = GetComponent<ReverseParticleSystemSimple>();
-
-        rainRotation = 0.7f;
+        _camera = Camera.main;
+        _particleSystem = GetComponent<ParticleSystem>();
+        _psShape = _particleSystem.shape;
+        _reverseParticleSystem = GetComponent<ReverseParticleSystemSimple>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-
-        // rot = Time.time * 40;
-        // rot = Mathf.Sin(Time.time/4) * 30;
-        // rot = -30;
-        // rot += Mathf.Clamp((Time.time-8f)*20, 0f, 60f);
-        // rot = Mathf.Sin(Time.time) * 50;
-        sh.rotation = new Vector3(0, 0, rainRotation*Mathf.Rad2Deg);
-        
-        if (cam == null) return;
-        var camPos = cam.transform.position;
-        camPos.x += Mathf.Cos(rainRotation+Mathf.PI*0.5f ) * distanceToCamera;
-        camPos.y += Mathf.Sin(rainRotation+Mathf.PI*0.5f ) * distanceToCamera;
+        if (!_camera) return;
+        var camPos = _camera.transform.position;
+        camPos.x += Mathf.Cos(rainRotation+Mathf.PI*0.5f ) * DistanceToCamera;
+        camPos.y += Mathf.Sin(rainRotation+Mathf.PI*0.5f ) * DistanceToCamera;
         camPos.z = transform.position.z;
-        sh.position = camPos;
+        _psShape.position = camPos;
     }
-
+    
     public void ChangeRainDirection(InputAction.CallbackContext ctx)
     {
-        if (ctx.canceled)
+        if (!ctx.performed)
             return;
-        Debug.Log("change direction");
-        // Debug.Log(ctx.phase);
-        Debug.Log(ctx.ReadValueAsObject());
-        // Debug.Log(Vector2.Angle(Vector2.zero, ctx.ReadValue<Vector2>()));
         
-        Debug.Log("Angle");
         var value = ctx.ReadValue<Vector2>();
         var rawRotation = Mathf.Atan2(value.y, value.x);
-        rainRotation = Mathf.Clamp(rawRotation + 1.57f, -1.57f, 1.57f);
+        var newRotation = (rawRotation + Mathf.PI*3/2) % (2*Mathf.PI) - Mathf.PI;
+        var clampedRotation = Mathf.Clamp(newRotation, -maxRainAngle, maxRainAngle);
+        DOTween.To(() => rainRotation, v => rainRotation = v, clampedRotation, rainChangeDuration);
     }
 
     public void ReverseRain(bool reversedDirection)
     {
         if (reversedDirection)
-           rv.StartReverse();
+           _reverseParticleSystem.StartReverse();
         else
-            rv.StopReverse();
+            _reverseParticleSystem.StopReverse();
     }
 }
